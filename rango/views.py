e5a055cba509fa481b2e09aaee1a07185879977a
Ella Anketell-Jones <2764057a@student.gla.ac.uk>
2024-02-02 13:36:28 +0000

    chapter 6 complete

--- rango/views.py
+++ rango/views.py
@@ -6,10 +6,12 @@ from rango.models import Page
 
 def index(request):
     category_list = Category.objects.order_by('-likes')[:5]
+    page_list = Page.objects.order_by('-views')[:5]
 
     context_dict = {}
     context_dict['boldmessage'] = 'Crunchy, creamy, cookie, candy, cupcake!'
     context_dict['categories'] = category_list
+    context_dict['pages'] = page_list
 
     #return a rendered response to send to the client.
     #We make use of the shortcut function to make out lives easier.
